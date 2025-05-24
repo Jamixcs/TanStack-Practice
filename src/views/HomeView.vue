199659561c7a521e0f20fbeeac57f68547313a71
Jia Guo <jamixcs@gmail.com>
2025-05-24 17:50:27 +0800

    refactor: remove useless code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,17 @@
-<script setup>
-import { ref } from 'vue'
-</script>
+<script setup></script>
 
 <template>
   <div class="flex flex-col gap-4 p-20">
-    <RouterLink to="/page1">Page1</RouterLink>
-    <RouterLink to="/page2">Page2</RouterLink>
+    <RouterLink
+      to="/page1"
+      class="inline-block bg-pink-400 hover:bg-pink-500 text-white font-semibold py-2 px-4 rounded transition duration-200"
+      >Page1</RouterLink
+    >
+    <RouterLink
+      to="/page2"
+      class="inline-block bg-pink-400 hover:bg-pink-500 text-white font-semibold py-2 px-4 rounded transition duration-200"
+      >Page2</RouterLink
+    >
   </div>
 </template>
 
